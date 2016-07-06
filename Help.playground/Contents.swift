6c4bb2ae8162d896e19637e6c872f2c7b677f2bc
imryan <notryancohen@gmail.com>
2016-07-06 13:07:13 -0400

    done

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -3,4 +3,5 @@ name.uppercaseString
 
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
-// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+// Damn...didn't she die in her sleep too?
